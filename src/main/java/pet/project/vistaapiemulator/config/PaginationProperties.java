587acfc32d6d9e7ba91d0cf6c7847e56a4d63b50
Hlib Leontiev <glebleontyev2003@gmail.com>
2025-09-04 13:37:34 +0300

    fix build.add setter to PaginationProperties

--- src/main/java/pet/project/vistaapiemulator/config/PaginationProperties.java
+++ src/main/java/pet/project/vistaapiemulator/config/PaginationProperties.java
@@ -1,12 +1,14 @@
 package pet.project.vistaapiemulator.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "pagination")
 @Getter
+@Setter
 public class PaginationProperties {
     private Integer defaultPage;
     private Integer defaultLimit;
